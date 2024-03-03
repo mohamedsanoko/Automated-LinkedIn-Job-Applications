06e8641842f33f3c1e734cd789f9ac8f9276740f
Mohamed <msanoko5@gmail.com>
2024-03-03 09:13:07 -0500

    Changing starting location and waiting for the page to fully load using time.sleep()

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3835957961&f_AL=true&geoId=101174742&keywords=data%20scientist&location=Canada&origin=JOB_SEARCH_PAGE_KEYWORD_AUTOCOMPLETE&refresh=true")
+driver.get("https://www.linkedin.com/feed/")
+
+time.sleep(5)
 
 
+# driver.implicitly_wait(10)
+# sign_in_button = driver.find_element(By.CSS_SELECTOR, value="div a")
+# sign_in_button.click()
